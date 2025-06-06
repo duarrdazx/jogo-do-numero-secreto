f330edaa69494e093c47549b432573139cd3b2c9
Eduarda Azevedo <eduardaazvdo9@gmail.com>
2025-06-05 21:30:18 -0300

    Deixando o jogo mais fácil

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
 let listaNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -26,7 +26,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 a 50');
 }
 
 exibirMensagemInicial();
